0f4abccf40067310c4e178fdad303e476fb61819
JuanCompaire <juancompaire2004@gmail.com>
2024-02-26 08:58:21 +0100

    se me olvidan los comits

--- exam.py
+++ exam.py
@@ -1,38 +1,86 @@
+from scp import SCPClient  
+from http.server import BaseHTTPRequestHandler, HTTPServer  
+import paramiko 
+from Crypto.PublicKey import RSA  
+from Crypto.Cipher import AES, PKCS1_OAEP  
+import smtplib  
+from ftplib import FTP  
 
-params = '',8083
+
+params = '', 8086  
+
 
 class HelloHandler(BaseHTTPRequestHandler):
-    def do_Head(self):
-        self.send_reponse(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
+    
+    def do_HEAD(self):
+        self.send_response(200)  
+        self.send_header('Content-type', 'text/html')  
+        self.end_headers()  
 
-    def do_Get(self):
-        self.do_Head()
+    def do_GET(self):
+        self.do_HEAD()  
+
         self.wfile.write("""
             <html><head>
-                <title>Examen Juan Compaire</title></head><body><p>HelloWorld</p>
+                <title>Examen </title></head><body><p>HelloWorld</p>
             <form method="POST" >
-            <input type="submit" value="Click me">
-                <img src="https://fotografias.lasexta.com/clipping/cmsimages01/2023/06/27/1E6C851B-C0AD-4C37-BA0F-8BE91B59FEAE/ilia-topuria_97.jpg?crop=1024,576,x0,y0&width=1600&height=900&optimize=low&format=webply">
+            <input type="submit" value="Probar">
+                <img src="https://s2.abcstatics.com/media/deportes/2020/10/08/hs-kzYF--1248x698@abc.jpg">
                         </input>
             </form>
-            </body></html>
+            </body></html>""".encode("utf-8"))  
 
-                         """.encode("utf-8"))
-        
-    def do_Post(self):
-        self.do_Head()
+    def do_POST(self):
+        self.do_HEAD()  
+
         self.wfile.write("""<html><head><title>Hello
             World</title></head><body><p>Form received</p>
-            </body></html>""".encode("utf-8"))
-        self.Examn()
+            </body></html>""".encode("utf-8"))  
+        self.Examn()  
+        
+    # Método para obtener documentos
+    def Examn(self):
+        print("Me ha llegado")
+        self.connectSSH()  
+        self.desEncrypt()  
+     #   self.writeEncryptedDataTofile()  # Escribe datos encriptados en un archivo
+     #   self.sendEmail()  # Envía un correo electrónico
+     #   self.uploadFTP()  # Sube archivos a través de FTP
+    
+    def connectSSH(self):
+        print("Conenctando a SSH")
 
-    def Examn():
-        print("SE hizo el examen")
-        pass
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-server = HTTPServer(params,HelloHandler)
+        ssh.connect(hostname='192.168.1.123', port=2222, username='tresdos', password='tresdos')
+        print("Conecté")
+
+        scp = SCPClient(ssh.get_transport())
+        scp.get('/home/encrypted_data.bin')
+        scp.get('/home/private.pem')
+        print("Descargé los files")
+        scp.close()
+        ssh.close()
+        
+        print("Finish SSH")
+    
+    def desEncrypt(self):
+        print("Desencriptar")
+        file_in = open("encrypted_data.bin","rb")
+        private_key = RSA.import_key(open("private.pem").read())
+        enc_session_key = file_in.read(private_key.size_in_bytes())
+        file_in.close()  
+
+        cipher_rsa = PKCS1_OAEP.new(private_key)
+        self.session_key = cipher_rsa.decrypt(enc_session_key)  
+        print(self.session_key)  
+        print("Desencriptar end")
+
+
+    
+server = HTTPServer(params, HelloHandler)
+
 
 try:
     server.serve_forever()
